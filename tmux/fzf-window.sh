c4a6c4200ba63575fad64cb6a4adfbd1e19d9dd8
j4hangir <j4hangir@gmail.com>
2026-04-07 18:38:58 +0200

    preselect current window, auto-accept on single match

--- tmux/fzf-window.sh
+++ tmux/fzf-window.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-window=$(tmux list-windows -F '#{window_index}: #{window_name}' | fzf-tmux -p)
+current=$(tmux display-message -p '#{window_index}')
+lines=$(tmux list-windows -F '#{window_index}: #{window_name}')
+pos=$(echo "$lines" | grep -n "^${current}:" | cut -d: -f1)
+window=$(echo "$lines" | fzf-tmux -p --no-sort --select-1 --bind "start:down(${pos:-1})+up")
 [ -n "$window" ] && tmux select-window -t "${window%%:*}"
