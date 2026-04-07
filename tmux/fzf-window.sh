88d07b01bda44d077525d01b753644faba56aa01
j4hangir <j4hangir@gmail.com>
2026-04-07 18:47:21 +0200

    fix fzf-window: repeat down instead of down(N)

--- tmux/fzf-window.sh
+++ tmux/fzf-window.sh
@@ -2,5 +2,12 @@
 current=$(tmux display-message -p '#{window_index}')
 lines=$(tmux list-windows -F '#{window_index}: #{window_name}')
 pos=$(echo "$lines" | grep -n "^${current}:" | cut -d: -f1)
-window=$(echo "$lines" | fzf-tmux -p --no-sort --select-1 --bind "start:down(${pos:-1})+up")
+moves=""
+i=1
+while [ "$i" -lt "${pos:-1}" ]; do
+  moves="${moves}+down"
+  i=$((i + 1))
+done
+moves="${moves#+}"
+window=$(echo "$lines" | fzf-tmux -p --no-sort --select-1 ${moves:+--bind "start:${moves}"})
 [ -n "$window" ] && tmux select-window -t "${window%%:*}"
