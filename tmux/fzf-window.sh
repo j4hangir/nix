5c38c82701235e8e0ac8f9e9a294b11b022776de
j4hangir <j4hangir@gmail.com>
2026-04-07 18:48:23 +0200

    fix fzf-window selection and auto-accept

--- tmux/fzf-window.sh
+++ tmux/fzf-window.sh
@@ -2,12 +2,15 @@
 current=$(tmux display-message -p '#{window_index}')
 lines=$(tmux list-windows -F '#{window_index}: #{window_name}')
 pos=$(echo "$lines" | grep -n "^${current}:" | cut -d: -f1)
+pos=$((${pos:-1} - 1))
 moves=""
-i=1
-while [ "$i" -lt "${pos:-1}" ]; do
+i=0
+while [ "$i" -lt "$pos" ]; do
   moves="${moves}+down"
   i=$((i + 1))
 done
 moves="${moves#+}"
-window=$(echo "$lines" | fzf-tmux -p --no-sort --bind "one:accept" ${moves:+--bind "start:${moves}"})
+bind="one:accept"
+[ -n "$moves" ] && bind="${bind},start:${moves}"
+window=$(echo "$lines" | fzf-tmux -p --no-sort --bind "$bind")
 [ -n "$window" ] && tmux select-window -t "${window%%:*}"
