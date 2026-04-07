219639962794349a8f3d9970f278dde423d1c2f7
j4hangir <j4hangir@gmail.com>
2026-04-07 18:50:19 +0200

    fix fzf-window: bottom-up selection with up moves

--- tmux/fzf-window.sh
+++ tmux/fzf-window.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 current=$(tmux display-message -p '#{window_index}')
 lines=$(tmux list-windows -F '#{window_index}: #{window_name}')
+total=$(echo "$lines" | wc -l | tr -d ' ')
 pos=$(echo "$lines" | grep -n "^${current}:" | cut -d: -f1)
-pos=$((${pos:-1} - 1))
+# fzf bottom-up: cursor starts at line 1 (bottom). target is (total - pos) ups from bottom.
+ups=$((total - ${pos:-1}))
 moves=""
 i=0
-while [ "$i" -lt "$pos" ]; do
-  moves="${moves}+down"
+while [ "$i" -lt "$ups" ]; do
+  moves="${moves}+up"
   i=$((i + 1))
 done
 moves="${moves#+}"
 bind="one:accept"
 [ -n "$moves" ] && bind="${bind},start:${moves}"
-window=$(echo "$lines" | fzf-tmux -p --no-sort --layout=reverse --bind "$bind")
+window=$(echo "$lines" | fzf-tmux -p --no-sort --bind "$bind")
 [ -n "$window" ] && tmux select-window -t "${window%%:*}"
