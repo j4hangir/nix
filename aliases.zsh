8fcd42024cc15c02e45bc7dad71651ccc2244c4e
j4hangir <j4hangir@gmail.com>
2026-04-13 18:42:42 +0200

    jailfree: detach instead of kill-session

--- aliases.zsh
+++ aliases.zsh
@@ -4,7 +4,7 @@ os=$("$NIXDIR/utils/os_detect.sh")
 # detach all other sessions but this one
 alias takeover="tmux detach -a"
 # kill tmux session, drop to bare shell (see configs/default_tmux.zshrc)
-alias jailfree='touch /tmp/.tmux_jailfree_$(id -u) && tmux kill-session'
+alias jailfree='touch /tmp/.tmux_jailfree_$(id -u) && tmux detach'
 
 # recursive and verbose
 alias mkdir="mkdir -pv"
